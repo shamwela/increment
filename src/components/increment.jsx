24af281dfee513f46f5119b9a0f853cec69a6c06
Sha Mwe La <shamwela2002@gmail.com>
2021-06-05 11:51:13 +0630

    Make clicking of the button to increment the count

--- src/components/increment.jsx
+++ src/components/increment.jsx
@@ -3,11 +3,17 @@ import React, { Component } from 'react';
 class Increment extends Component {
   state = { count: 0 };
 
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     return (
       <React.Fragment>
-        <span>{this.state.count}</span>
-        <button>+</button>
+        <span style={{ margin: '3rem' }}>{this.state.count}</span>
+        <button style={{ margin: '3rem' }} onClick={this.handleIncrement}>
+          +
+        </button>
       </React.Fragment>
     );
   }
